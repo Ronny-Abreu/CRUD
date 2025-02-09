2f9b685b4cb38b9c1c6232d1f65ac25f2d76ffc5
SlimDReaper <ronnyduran35@gmail.com>
2025-02-08 22:33:07 -0400

    Migration Db files

--- CrudProyecto/Data/ApplicationDbContext.cs
+++ CrudProyecto/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using CrudProyecto.Models;
 
 namespace CrudProyecto.Data
 {
@@ -9,5 +10,7 @@ namespace CrudProyecto.Data
             : base(options)
         {
         }
+
+        public DbSet<Producto> Productos { get; set; }
     }
 }
--- CrudProyecto/Data/Migrations/20250209022858_InitialMigrations.Designer.cs
+++ CrudProyecto/Data/Migrations/20250209022858_InitialMigrations.Designer.cs
@@ -4,6 +4,7 @@ using CrudProyecto.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CrudProyecto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250209022858_InitialMigrations")]
+    partial class InitialMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
